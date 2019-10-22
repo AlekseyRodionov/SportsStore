aa1805d03c166ab87264c7d466cf27649146c86c
Илья Родионов <ilia.rodionov20021801@outlook.com>
2019-10-22 19:28:29 +0300

    Добавлен контролер передающий данные из хранилища Product в стандартное представление.

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController: Controller
+    {
+        //Получаем данные из хранилища
+        private IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        //Передаем данные в стандартное представление 
+        public ViewResult List() => View(repository.Products);
+    }
+}
