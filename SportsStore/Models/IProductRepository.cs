d326c71f4f706963b8c4a1479974f260999b56e9
Илья Родионов <ilia.rodionov20021801@outlook.com>
2019-10-22 11:45:40 +0300

    Добавлен интерфейс для работы с моделью, фиктивное хранилище.

---
+++ SportsStore/Models/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+
+namespace SportsStore.Models
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> Products { get; } // получаем коллекцию Product
+    }
+}
