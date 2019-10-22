35087329bbcae21a0c11d372389c97e83530cf2b
Илья Родионов <ilia.rodionov20021801@outlook.com>
2019-10-23 01:01:52 +0300

    Подготовка и настройка БД, подключение к БД.

---
+++ SportsStore/Models/SeedData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                     new Product
+                     {
+                         Name = "Lifejacket",
+                         Description = "A boat for one person",
+                         Category = "Watersports",
+                         Price = 48.95m
+                     },
+                     new Product
+                     {
+                         Name = "Bench Press",
+                         Description = "used for weight lifting",
+                         Category = "Fitness",
+                         Price = 20.95m
+                     },
+                      new Product
+                      {
+                          Name = "Punching Bag",
+                          Description = "used for training",
+                          Category = "Boxing",
+                          Price = 20.95m
+                      },
+                      new Product
+                      {
+                          Name = "Soccer Ball",
+                          Description = "A boat for one person",
+                          Category = "Sports",
+                          Price = 19.50m
+                      },
+                      new Product
+                      {
+                          Name = "Soccer Ball Youth",
+                          Description = "A boat for one person",
+                          Category = "Sports",
+                          Price = 19.50m
+                      },
+                      new Product
+                      {
+                          Name = "BaseBall",
+                          Description = "A boat for one person",
+                          Category = "Sports",
+                          Price = 19.50m
+                      },
+                      new Product
+                      {
+                          Name = "Round Ball",
+                          Description = "A boat for one person",
+                          Category = "Sports",
+                          Price = 19.50m
+                      }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -7,16 +7,22 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
+            //services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
         }
 
@@ -31,6 +37,7 @@ namespace SportsStore
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
             }); // включает MVC, маршрутизация 
+            SeedData.EnsurePopulated(app);
         }
     }
 }
